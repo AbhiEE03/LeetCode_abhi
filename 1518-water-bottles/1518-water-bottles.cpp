29b537bc214979a0f81ee97f8b07f275506b72ae
Abhishek <abhisheknoni78@gmail.com>
2025-10-04 12:26:40 +0530

    Time: 0 ms (100%), Space: 7.8 MB (44.95%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int drunk=numBottles;
-        int empty=numBottles;
 
-        while(empty>=numExchange){
-            empty-=numExchange;
-            drunk++;
-            empty++;
-        }
-        return drunk;
+        return numBottles + (numBottles-1)/(numExchange-1);
     }
 };
+
+
+// class Solution {
+// public:
+//     int numWaterBottles(int numBottles, int numExchange) {
+//         int drunk=numBottles;
+//         int empty=numBottles;
+
+//         while(empty>=numExchange){
+//             empty-=numExchange;
+//             drunk++;
+//             empty++;
+//         }
+//         return drunk;
+//     }
+// };
