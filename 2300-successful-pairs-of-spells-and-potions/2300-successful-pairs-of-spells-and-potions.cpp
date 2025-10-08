d731d911b87a2e8b5a8a4764df48c1e8e2c0cae6
Abhishek <abhisheknoni78@gmail.com>
2025-10-09 01:57:01 +0530

    Time: 39 ms (84.14%), Space: 128.4 MB (25.65%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,15 +1,42 @@
 class Solution {
 public:
+
+    int valid_pos(int spell, vector<int>& potions, long long success){
+        long long potions_needed=(success + spell-1 )/spell;
+        int l=0,r=potions.size();
+        while(l<r){
+            int mid= l+(r-l)/2;
+            if (potions[mid]>= potions_needed)
+                r=mid;
+            else
+                l=mid+1;
+        }
+        return l;  
+    }
+
+
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        // sort(spells.begin(), spells.end());
         sort(potions.begin(), potions.end());
         vector<int> res;
 
         for(auto spell:spells){
-            long long potions_needed = (success + spell-1)/spell;
-            auto temp = lower_bound(potions.begin(), potions.end(), potions_needed);
-            res.push_back(potions.end()-temp);
+            res.push_back(potions.size()-valid_pos(spell, potions,success));
         }
         return res;
     }
 };
+
+
+// Using C++ STL
+// vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+//         // sort(spells.begin(), spells.end());
+//         sort(potions.begin(), potions.end());
+//         vector<int> res;
+
+//         for(auto spell:spells){
+//             long long potions_needed = (success + spell-1)/spell;
+//             auto temp = lower_bound(potions.begin(), potions.end(), potions_needed);
+//             res.push_back(potions.end()-temp);
+//         }
+//         return res;
+//     }
