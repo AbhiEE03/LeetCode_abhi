e1131345395e2870c2690c36d66af7561e545709
Abhishek <abhisheknoni78@gmail.com>
2025-10-11 00:51:15 +0530

    Time: 137 ms (68.66%), Space: 151.8 MB (24.3%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
-        int n=energy.size(), maxEnergy=INT_MIN;
+        int n=energy.size();
+        vector<int> prefix(n,0);
 
-        for(int i=0; i<n; i++){
-            int temp=energy[i];
-            for(int j=i+k; j<n; j+=k){
-                temp+=energy[j];
-            }
-
-            maxEnergy = max(maxEnergy, temp);
-
+        for(int i=n-1; i>=0; i--){
+            prefix[i]=energy[i];
+            
+           if(i+k < n){
+            prefix[i]= prefix[i+k] + energy[i];
+           }
         }
-        return maxEnergy;
+
+        return *max_element(prefix.begin(), prefix.end());
     }
 };
+
+
+// // BRUTE FORCE // 
+// class Solution {
+// public:
+//     int maximumEnergy(vector<int>& energy, int k) {
+//         int n=energy.size(), maxEnergy=INT_MIN;
+
+//         for(int i=0; i<n; i++){
+//             int temp=energy[i];
+            
+//             for(int j=i+k; j<n; j+=k){
+//                 temp+=energy[j];
+//             }
+
+//             maxEnergy = max(maxEnergy, temp);
+
+//         }
+//         return maxEnergy;
+//     }
+// };
