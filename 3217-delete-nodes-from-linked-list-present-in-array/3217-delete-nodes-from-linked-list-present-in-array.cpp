b392c63e1627599ee03c631e1a1ed204fdab7cc7
Abhishek <abhisheknoni78@gmail.com>
2025-11-01 19:19:47 +0530

    Time: 99 ms (59.6%), Space: 263.5 MB (41.15%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.cpp
@@ -14,7 +14,7 @@ public:
         unordered_set<int> st(nums.begin(), nums.end());
 
         while(head!=NULL && st.find(head->val)!= st.end()){
-            ListNode *temp = head;
+            // ListNode *temp = head;
             head = head->next;
             // delete(temp);
         }
@@ -22,7 +22,7 @@ public:
         ListNode *curr= head;
         while(curr!=NULL && curr->next!=NULL){
             if(st.find(curr->next->val) != st.end()){
-                ListNode* temp = curr->next;
+                // ListNode* temp = curr->next;
                 curr->next = curr->next->next;
                 // delete(temp);
             }else
@@ -32,3 +32,10 @@ public:
         return head;
     }
 };
+
+// We don't need to delete the nodes manually beacuse leetcode does it on its own 
+// and if we are doing it in our code then leetcode will try to delete it again 
+// and we will get runtime error.
+
+// We are handling the head first and then moving to curr-> next.
+// Keep moving the current untill u hit null and keep breaking the linkage if curr->next is in the set
