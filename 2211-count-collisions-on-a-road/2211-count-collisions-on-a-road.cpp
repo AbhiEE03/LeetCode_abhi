9456c3950b3a422c871bb85a3203136e2d280b2a
Abhishek <abhisheknoni78@gmail.com>
2025-12-04 21:31:24 +0530

    Time: 7 ms (82.03%), Space: 19.3 MB (64.06%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -4,20 +4,17 @@ public:
         long long collisions=0, n = directions.size();
         long long l = 0, r = n-1;
         // Eliminating left moving cars on the left boundary
-        while(directions[l] == 'L' && l<n){
+        while( l<n && directions[l] == 'L' ){
             l++;
         }
          // Eliminating right moving cars on the right boundary
-        while(directions[r]=='R' && r>=0){
+        while(r>=0 && directions[r]=='R' ){
             r--;
         }
 
         // Tackling those which will contribute to collision
-        while(l<=r){
-            if(directions[l] =='R' || directions[l] == 'L' ){
-                collisions++;
-                l++;
-            }else l++;
+        for(int i=l; i<=r; i++){
+            if(directions[i]!='S') collisions++;
         }
 
         return collisions;
