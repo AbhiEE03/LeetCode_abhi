94865604a26e62c6cf5deedde4028a56d2a4975d
Abhishek <abhisheknoni78@gmail.com>
2025-12-20 16:49:48 +0530

    Time: 0 ms (100%), Space: 17.7 MB (96.02%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,47 +1,97 @@
+
+// //// USING BFS //// //
 class Solution {
-
-private:
-    bool dfs_topological(int course, vector<int> adj[], vector<int> &visited, vector<int> &path_visited, vector<int> &topological_order){
-        visited[course] = 1;
-        path_visited[course] = 1;
-
-        for(auto &it : adj[course]){
-            if(!visited[it]){
-                if(dfs_topological(it, adj, visited, path_visited, topological_order)) return true;
-            }else if(path_visited[it] == 1) return true;
-        }
-
-        topological_order.push_back(course);
-
-        // Reset the visited path as we are returning
-        path_visited[course] = 0;
-        return false;
-    }
-
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> visited (numCourses, 0);
-        vector<int> path_visited(numCourses, 0);
-        vector<int>topological_order;
-
-        // Creating an adjacency List
+        // Creating an anjacency matrix
         vector<int> adj[numCourses];
-
         for(auto &it : prerequisites){
             int c = it[0];
             int p = it[1];
-
             adj[p].push_back(c);
+            // adj[it.first].push_back(it.second);
         }
 
-        // For all the vertices so that we don't miss the componenets which are not connected
+        // Creating indegree vector
+        vector<int> indegree(numCourses,0);
         for(int i=0; i<numCourses; i++){
-            if(!visited[i]){
-                if(dfs_topological(i, adj, visited, path_visited, topological_order)) return {};
-            }
+            for(auto &it:adj[i])
+                indegree[it]++;
         }
 
-        reverse(topological_order.begin(), topological_order.end());
-        return topological_order;
+        // Storing the nodes having indegree==0 in the queue
+        queue<int> q;
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i]==0)
+                q.push(i);
+        }
+
+        vector<int> topo;
+        int count=0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            count++;
+
+            for(auto &it : adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }   
+        }
+
+        if(count==numCourses) return topo;
+        return {};
+
     }
 };
+
+
+// // //// USING DFS //// //
+// class Solution {
+
+// private:
+//     bool dfs_topological(int course, vector<int> adj[], vector<int> &visited, vector<int> &path_visited, vector<int> &topological_order){
+//         visited[course] = 1;
+//         path_visited[course] = 1;
+
+//         for(auto &it : adj[course]){
+//             if(!visited[it]){
+//                 if(dfs_topological(it, adj, visited, path_visited, topological_order)) return true;
+//             }else if(path_visited[it] == 1) return true;
+//         }
+
+//         topological_order.push_back(course);
+
+//         // Reset the visited path as we are returning
+//         path_visited[course] = 0;
+//         return false;
+//     }
+
+// public:
+//     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<int> visited (numCourses, 0);
+//         vector<int> path_visited(numCourses, 0);
+//         vector<int>topological_order;
+
+//         // Creating an adjacency List
+//         vector<int> adj[numCourses];
+
+//         for(auto &it : prerequisites){
+//             int c = it[0];
+//             int p = it[1];
+
+//             adj[p].push_back(c);
+//         }
+
+//         // For all the vertices so that we don't miss the componenets which are not connected
+//         for(int i=0; i<numCourses; i++){
+//             if(!visited[i]){
+//                 if(dfs_topological(i, adj, visited, path_visited, topological_order)) return {};
+//             }
+//         }
+
+//         reverse(topological_order.begin(), topological_order.end());
+//         return topological_order;
+//     }
+// };
