7fd93badf75656beb46548cb2b5cd53b3250819f
Abhishek <abhisheknoni78@gmail.com>
2025-12-27 02:41:25 +0530

    Time: 160 ms (45.99%), Space: 107.7 MB (88.12%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -2,27 +2,44 @@ class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         sort(happiness.begin(), happiness.end(), [](int a, int b){
-            return a>b;
+            return a > b;
         });
 
-        long long s=happiness.size();
-
-        long long sum = 0, i=0,turn = 0;
-        while(k>0){
-            long long curr = happiness[i]-turn;
-            // sum = sum + max(0, happiness[i]-turn);
-            if(curr <= 0) break;
-
-            sum+=curr;
-            k--;
-            i++;
-            turn++;
-            // for(long long j=i+1; j<s; j++){
-            //     if(happiness[j]>0){
-            //         happiness[j]-=1;
-            //     }
-            // }
+        long long sum=0;
+        for(long long i=0; i<k; i++){
+            sum = sum + max(happiness[i]-i, (long long)0);
         }
         return sum;
     }
 };
+
+
+// //// Alternate //// //
+// class Solution {
+// public:
+//     long long maximumHappinessSum(vector<int>& happiness, int k) {
+//         sort(happiness.begin(), happiness.end(), [](int a, int b){
+//             return a>b;
+//         });
+
+//         long long s=happiness.size();
+
+//         long long sum = 0, i=0,turn = 0;
+//         while(k>0){
+//             long long curr = happiness[i]-turn;
+//             // sum = sum + max(0, happiness[i]-turn);
+//             if(curr <= 0) break;
+
+//             sum+=curr;
+//             k--;
+//             i++;
+//             turn++;
+//             // for(long long j=i+1; j<s; j++){
+//             //     if(happiness[j]>0){
+//             //         happiness[j]-=1;
+//             //     }
+//             // }
+//         }
+//         return sum;
+//     }
+// };
