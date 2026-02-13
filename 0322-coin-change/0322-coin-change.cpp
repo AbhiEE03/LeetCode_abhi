29432961867af1970ec7293911034aed5b80ae7a
Abhishek <abhisheknoni78@gmail.com>
2026-02-14 01:19:34 +0530

    Time: 51 ms (24.94%), Space: 53 MB (12.94%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -5,12 +5,15 @@ public:
         int n = coins.size();
         vector<vector<int>> dp(n+1, vector<int> (amount+1, 0));
 
+        // You can never have amount+1 no. of coins for a valid amount
+        //  because the minimum value of coin is 1
         for(int j=1; j<amount+1; j++)
             dp[0][j] = amount+1;
 
         for(int i=1; i<n+1; i++){
             for(int j=1; j<amount+1; j++){
             
+            // The coin giving valid ans can be taken again
             if(coins[i-1] <= j)
                 dp[i][j] = min(dp[i][j-coins[i-1]] + 1, dp[i-1][j]);
             else
@@ -18,6 +21,7 @@ public:
             }
         }
 
+        // If it has >= amount+1 coins then return -1
         return dp[n][amount] >= amount+1 ? -1 : dp[n][amount];
     }
 };
