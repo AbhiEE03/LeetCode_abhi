6f1d26eb0e32e3258852061342186f6949b54c17
Abhishek <abhisheknoni78@gmail.com>
2026-02-20 23:49:41 +0530

    Time: 31 ms (66.04%), Space: 31.6 MB (16.87%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -3,11 +3,16 @@ public:
     int minInsertions(string s) {
         int n = s.length();
 
+        //  Reversing directly won't help because reverse doesn't return anything 
+        // So store s in a new string s1 and reverse it 
+        // The apply LCS(s, s1)
         string s1 = s;
         reverse(s1.begin(), s1.end());
 
+        // Initialisation
         vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
 
+        //  Code for LCS(ab to yaad ho gya)
         for( int i=1; i<n+1; i++){
              for( int j =1; j<n+1; j++){
                 if(s[i-1] == s1[j-1])
@@ -17,6 +22,7 @@ public:
              }
         }
 
+        // Min No. of insertions = Min No. of deletions = s.length - LCS
         return n - dp[n][n];
     }
 };
