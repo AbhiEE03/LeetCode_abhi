b3023b87719ea063bac149b82b69e34377446daa
Abhishek <abhisheknoni78@gmail.com>
2026-02-22 17:13:51 +0530

    Time: 0 ms (100%), Space: 7.9 MB (54.7%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, 1);
-        // if(n==1) return 1;
-        // if(n==2) return 2;
-        // Base Case
-        // dp[0] = 0;
-        // dp[1] = 1;
-        // dp[2] = 2;
-
-        int ways = 0;
+    // Space optimized solution
+    // We need only the prev and prev2 data
+        int prev = 1, prev2 = 1;
         for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr_stair = prev + prev2;
+            prev2 = prev;
+            prev = curr_stair;
         }
-        return dp[n];
+        return prev;
     }
 };
+
+
+// //// 1D dp solution //// //
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         // Initialisation
+//         // There is only 1 way to be at 0th and 1st index (Base Case)
+//         vector<int> dp(n+1, 1);
+        
+//         // Base Case which are already initialised
+//         // dp[0] = 1;
+//         // dp[1] = 1;
+
+//         //  Total Ways = sum of ways to reach the prev and prev2 stairs
+//         for(int i=2; i<=n; i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// };
