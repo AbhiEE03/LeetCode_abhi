637590c66777547b1706900cd87efbf93f129c51
Abhishek <abhisheknoni78@gmail.com>
2026-02-26 02:35:53 +0530

    Time: 0 ms (100%), Space: 16.2 MB (70.46%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -3,25 +3,29 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        // Initislising by zero has no real significance, it's just a placeholder because 
+        // it's getting overwritten anyways
         vector<vector<int>>dp(m, vector<int>(n, 0));
 
+        // Base Case
+        dp[m-1][n-1] = grid[m-1][n-1];
         // When we are in the last row : We can go only rightwards(1 way)
         // When we are in the last col : We can go only downwards(1 way)
-        dp[m-1][n-1] = grid[m-1][n-1];
         for(int i=m-1; i>=0; i--){
             for(int j=n-1; j>=0; j--){
                 if(i == m-1 && j == n-1) continue;
-                // Change the dp state only when we move out of last row and last col
-                if(i<m-1 && j<n-1)
-                    dp[i][j] = min(dp[i+1][j], dp[i][j+1]) + grid[i][j];
-                    // We can either go right or left at each step
-                else if(i==m-1)
-                    dp[i][j] = dp[i][j+1] + grid[i][j];
-                else
-                    dp[i][j] = dp[i+1][j] + grid[i][j];
+                // We will have the contribution of down in last col
+                // right in last row
+                // down + right in all other cells
+                int down  = (i < m-1) ? dp[i+1][j] : INT_MAX;
+                int right = (j < n-1) ? dp[i][j+1] : INT_MAX;
+
+                dp[i][j] = grid[i][j] + min(down, right);
             }
         }
-        // The total no. of ways to go from 0,0 to m-1,n-1
+        // Min Path sum to go from 0,0 to m-1,n-1
         return dp[0][0];
     }
 };
+// If they had asked for max path sum then we just needed to change 23rd line:
+// dp[i][j] = grid[i][j] + max(down, right);
