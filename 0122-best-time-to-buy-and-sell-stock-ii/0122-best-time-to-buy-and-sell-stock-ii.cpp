f013a76fb8336de5cf751b35cc2354d267a87c8c
Abhishek <abhisheknoni78@gmail.com>
2026-03-07 13:55:58 +0530

    Time: 9 ms (19.28%), Space: 24.8 MB (14.41%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        // Initialisation
+        // size[n+1][2] with 0 profit
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+
+        // Base Case
+        // After reaching the end, we can't buy or sell anything
+        dp[n][0] = dp[n][1] = 0; 
+
+        for(int i = n-1; i>=0; i--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy){
+                    // buying curr + sold prev, didn't buy curr + bought the prev
+                    profit = max(-prices[i] + dp[i+1][0], 0 + dp[i+1][1]);
+                }else{
+                    // selling curr + bought prev, didn't sell curr + sold the prev
+                    profit = max(prices[i] + dp[i+1][1], 0 + dp[i+1][0]);
+                }
+                // Assign the profit to the current dp state
+                dp[i][buy] = profit;
+            }
+        }
+        // return the last dpp state
+        return dp[0][1];
+    }
+};
