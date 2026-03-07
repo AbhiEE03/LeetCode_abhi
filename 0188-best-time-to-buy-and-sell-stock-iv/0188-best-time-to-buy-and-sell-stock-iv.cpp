15111a8f78c264ff97ad165133923cc7eef16eec
Abhishek <abhisheknoni78@gmail.com>
2026-03-07 18:00:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,11 +3,13 @@ public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
 
+        // Since the max transactions allowed is k so we need to initialise by k+1
         vector<vector<int>>next_state(2, vector<int>(k+1, 0));
         vector<vector<int>>curr_state(2, vector<int>(k+1, 0));
 
         for(int i = n-1; i >= 0; i--){
             for(int buy = 0; buy <= 1; buy++){
+                // Cap = k
                 for(int cap = 1; cap <= k; cap++){
                     if(buy){
                         curr_state[buy][cap] = max(-prices[i] + next_state[0][cap],
