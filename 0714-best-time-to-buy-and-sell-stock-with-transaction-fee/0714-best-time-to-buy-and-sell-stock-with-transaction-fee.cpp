e277e401cf33b6e967cf1a784e3b39494dad6234
Abhishek <abhisheknoni78@gmail.com>
2026-03-07 19:29:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -26,30 +26,55 @@
 
 
 
-//  //// DP with space optimisation //// //
+// //  //// DP with space optimisation //// //
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices, int fee) {
+//         int n = prices.size();
+//         // Initialisation
+//         // 2 vectors for storing the next_state and temp_state
+//         vector<int> next_state(2, 0), curr_state(2,0);
+
+//         // No stocks = No profit
+//         // next_state[0] = next_state[1] = 0; (Not required)
+
+//         for(int i = n-1; i >= 0; i--){
+
+//             // buying curr + selling next, didn't buy curr + buying the next
+//             curr_state[1] = max(-prices[i] + next_state[0], next_state[1]);
+
+//             // selling curr + buying next, didn't sell curr + selling the next
+//             curr_state[0] = max(prices[i] + next_state[1] - fee, next_state[0]);
+
+//             // store the temp as next state for coming iteration
+//             next_state = curr_state;
+//         }
+//         return next_state[1];
+//     }
+// };
+
+
+
+// //// Using 4 variables //// //
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        // Initialisation
-        // 2 vectors for storing the next_state and temp_state
-        vector<int> next_state(2, 0), curr_state(2,0);
 
-        // No stocks = No profit
-        // next_state[0] = next_state[1] = 0; (Not required)
+        // Initialise the variables
+        int curr_buy, curr_sell, next_buy, next_sell;
+
+        next_buy = next_sell = 0;
 
         for(int i = n-1; i >= 0; i--){
-            
-            // buying curr + selling next, didn't buy curr + buying the next
-            curr_state[1] = max(-prices[i] + next_state[0], next_state[1]);
-
-            // selling curr + buying next, didn't sell curr + selling the next
-            curr_state[0] = max(prices[i] + next_state[1] - fee, next_state[0]);
-
-            // store the temp as next state for coming iteration
-            next_state = curr_state;
+                
+            curr_buy = max(-prices[i] + next_sell, next_buy);
+            curr_sell = max(prices[i] - fee + next_buy, next_sell);
+                
+            next_buy = curr_buy;
+            next_sell = curr_sell;
         }
-        return next_state[1];
+
+        return next_buy;
     }
 };
-
