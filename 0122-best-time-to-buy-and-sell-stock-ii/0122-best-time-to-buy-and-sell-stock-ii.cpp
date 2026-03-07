66177b79ecf187559e4f469bfc7202b42f8c253a
Abhishek <abhisheknoni78@gmail.com>
2026-03-07 19:05:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,69 +1,60 @@
 
 
 
-//// Tabulation w/o space optimisation //// //
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        // Initialisation
-        // size[n+1][2] with 0 profit
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
-
-        // Base Case
-        // After reaching the end, we can't buy or sell anything
-        // dp[n][0] = dp[n][1] = 0; (Not required)
-
-        for(int i = n-1; i>=0; i--){
-            // buying curr + sold next, didn't buy curr + buying the next
-            dp[i][1] = max(-prices[i] + dp[i + 1][0], 0 + dp[i + 1][1]);
-
-            // selling curr + buying next, didn't sell curr + selling the next
-            dp[i][0] = max(prices[i] + dp[i + 1][1], 0 + dp[i + 1][0]);
-        }
-        // return the last dp state
-        return dp[0][1];
-    }
-};
-
-
-
-
-// //// Space Optimized //// //
+// //// Tabulation w/o space optimisation //// //
 // class Solution {
 // public:
 //     int maxProfit(vector<int>& prices) {
 //         int n = prices.size();
 //         // Initialisation
-//         // 2 vectors for storing the next_state and temp_state
-//         vector<int> next_state(2, 0), curr(2,0);
+//         // size[n+1][2] with 0 profit
+//         vector<vector<int>> dp(n+1, vector<int>(2, 0));
 
-//         // No stocks = No profit
-//         next_state[0] = next_state[1] = 0;
+//         // Base Case
+//         // After reaching the end, we can't buy or sell anything
+//         // dp[n][0] = dp[n][1] = 0; (Not required)
 
-//         for(int i = n-1; i >= 0; i--){
-//             for(int buy = 0; buy <= 1; buy++){
-//                 int profit = 0;
-//                 if(buy){
-//                     // profit = buy curr + sell next, buy next
-//                     profit = max(-prices[i] + next_state[0], next_state[1]);
-//                 }else{
-//                     // profit = sell curr + buy next, sell next
-//                     profit = max(prices[i] + next_state[1], next_state[0]);
-//                 }
-//                 // Assign profit in the temp var
-//                 curr[buy] = profit;
-//             }
-//             // store the temp as next state for coming iteration
-//             next_state = curr;
+//         for(int i = n-1; i>=0; i--){
+//             // buying curr + selling next, didn't buy curr + buying the next
+//             dp[i][1] = max(-prices[i] + dp[i + 1][0], 0 + dp[i + 1][1]);
+
+//             // selling curr + buying next, didn't sell curr + selling the next
+//             dp[i][0] = max(prices[i] + dp[i + 1][1], 0 + dp[i + 1][0]);
 //         }
-
-//     // Return the last state
-//     return next_state[1];
+//         // return the last dp state
+//         return dp[0][1];
 //     }
 // };
 
-// };
+
+
+
+//// Space Optimized //// //
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        // Initialisation
+        // 2 vectors for storing the next_state and temp_state
+        vector<int> next_state(2, 0), curr_state(2,0);
+
+        // No stocks = No profit
+        // next_state[0] = next_state[1] = 0; (Not required)
+
+        for(int i = n-1; i >= 0; i--){
+            // buying curr + selling next, didn't buy curr + buying the next
+            curr_state[1] = max(-prices[i] + next_state[0], 0 + next_state[1]);
+
+            // selling curr + buying next, didn't sell curr + selling the next
+            curr_state[0] = max(prices[i] + next_state[1], 0 + next_state[0]);
+            // store the temp as next state for coming iteration
+            next_state = curr_state;
+        }
+
+    // Return the last state
+    return next_state[1];
+    }
+};
 
 
 
