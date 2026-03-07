0a20e3d2b0f074b1b93bdb54e06d31526ad7d8db
Abhishek <abhisheknoni78@gmail.com>
2026-03-07 17:21:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,56 @@
+// // //// Dp w/o space optimisation //// //
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         // Initialisation
+//         // If (cap==0 || index == n) return 0
+//         // Base Case
+//         vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+//         for(int i=n-1; i>=0; i--){
+//             for(int buy=0; buy<=1; buy++){
+//                 for(int cap=1; cap<=2; cap++){
+//                     // Capacity decrease only when we sell the stock
+//                     if(buy){
+//                         dp[i][buy][cap] = max(-prices[i] + dp[i+1][0][cap], 0 + dp[i+1][1][cap]);
+//                     }else{
+//                         dp[i][buy][cap] = max(prices[i] + dp[i+1][1][cap-1], 0 + dp[i+1][0][cap]);
+//                     }
+//                 }
+//             }
+//         }
+//         // return the last state where elements=0, buy=1,
+//         return dp[0][1][2];
+//     }
+// };
+
+
+
+// //// Dp with space optimisation //// //
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        // Initialisation
-        // If (cap==0 || index == n) return 0
-        // Base Case
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
 
-        for(int i=n-1; i>=0; i--){
-            for(int buy=0; buy<=1; buy++){
-                for(int cap=1; cap<=2; cap++){
-                    // Capacity decrease only when we sell the stock
+        vector<vector<int>>next_state(2, vector<int>(3, 0));
+        vector<vector<int>>curr_state(2, vector<int>(3, 0));
+
+        for(int i = n-1; i >= 0; i--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= 2; cap++){
                     if(buy){
-                        dp[i][buy][cap] = max(-prices[i] + dp[i+1][0][cap], 0 + dp[i+1][1][cap]);
+                        curr_state[buy][cap] = max(-prices[i] + next_state[0][cap],
+                        next_state[1][cap]);
                     }else{
-                        dp[i][buy][cap] = max(prices[i] + dp[i+1][1][cap-1], 0 + dp[i+1][0][cap]);
+                        curr_state[buy][cap] = max(prices[i] + next_state[1][cap-1], 
+                        next_state[0][cap]);
                     }
                 }
             }
+            next_state = curr_state;
         }
-        // return the last state where elements=0, buy=1, cap=2
-        return dp[0][1][2];
+
+        return next_state[1][2];
     }
 };
