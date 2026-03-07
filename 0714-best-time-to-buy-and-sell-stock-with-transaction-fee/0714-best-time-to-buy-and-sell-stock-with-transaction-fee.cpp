e50347ff89acf38022bd3c599afd6856657438fe
Abhishek <abhisheknoni78@gmail.com>
2026-03-07 19:23:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,26 +1,55 @@
-//// Tabulation w/o space optimisation //// //
+// //// Tabulation w/o space optimisation //// //
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices, int fee) {
+//         int n = prices.size();
+//         // Initialisation
+//         // size[n+1][2] with 0 profit bec we are accesing i+1 after selling
+//         vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+//         // Base Case
+//         // After reaching the end, we can't buy or sell anything
+//         // dp[n][0] = dp[n][1] = 0; (NOt required bec we are initialising with 0)
+
+//         for (int i = n - 1; i >= 0; i--) {
+
+//             // buying curr + selling next, didn't buy curr + buying the next
+//             dp[i][1] = max(-prices[i] + dp[i + 1][0], 0 + dp[i + 1][1]);
+
+//             // selling curr + buying next, didn't sell curr + selling the next
+//             dp[i][0] = max(prices[i] + dp[i + 1][1] - fee, 0 + dp[i + 1][0]);
+//         }
+//         // return the last dpp state
+//         return dp[0][1];
+//     }
+// };
+
+
+
+//  //// DP with space optimisation //// //
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
         // Initialisation
-        // size[n+1][2] with 0 profit bec we are accesing i+1 after selling
-        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        // 2 vectors for storing the next_state and temp_state
+        vector<int> next_state(2, 0), curr_state(2,0);
 
-        // Base Case
-        // After reaching the end, we can't buy or sell anything
-        // dp[n][0] = dp[n][1] = 0; (NOt required bec we are initialising with 0)
+        // No stocks = No profit
+        // next_state[0] = next_state[1] = 0; (Not required)
 
-        for (int i = n - 1; i >= 0; i--) {
-
+        for(int i = n-1; i >= 0; i--){
+            
             // buying curr + selling next, didn't buy curr + buying the next
-            dp[i][1] = max(-prices[i] + dp[i + 1][0], 0 + dp[i + 1][1]);
+            curr_state[1] = max(-prices[i] + next_state[0], next_state[1]);
 
             // selling curr + buying next, didn't sell curr + selling the next
-            dp[i][0] = max(prices[i] + dp[i + 1][1] - fee, 0 + dp[i + 1][0]);
+            curr_state[0] = max(prices[i] + next_state[1] - fee, next_state[0]);
+
+            // store the temp as next state for coming iteration
+            next_state = curr_state;
         }
-        // return the last dpp state
-        return dp[0][1];
+        return next_state[1];
     }
 };
 
