64f3266917c10a10252d4743f1d5304f4c680c6c
Abhishek <abhisheknoni78@gmail.com>
2026-06-02 23:16:17 +0530

    Time: 0 ms (100%), Space: 97.4 MB (59.91%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,38 @@
+// Revision via SDESheetChallenge
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int mini = prices[0]; 
-        int n = prices.size();
-
         int profit = 0;
-        for(int i=1; i<n; i++){
-            // Profit after selling the stock on ith day
-            int curr_val = prices[i] - mini;
-            // Track the max profit
-            profit = max(profit, curr_val);
-            // Keep track of the min value where u can buy the stock
-            mini = min(mini, prices[i]);
-        }
+        int minBuyingPrice = prices[0];
 
+        for(int i = 1; i < prices.size(); i++){
+            int currProfit = prices[i] - minBuyingPrice;
+            profit = max(currProfit, profit);
+
+            minBuyingPrice = min(minBuyingPrice, prices[i]);
+        }
         return profit;
     }
 };
+
+
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int mini = prices[0]; 
+//         int n = prices.size();
+
+//         int profit = 0;
+//         for(int i=1; i<n; i++){
+//             // Profit after selling the stock on ith day
+//             int curr_val = prices[i] - mini;
+//             // Track the max profit
+//             profit = max(profit, curr_val);
+//             // Keep track of the min value where u can buy the stock
+//             mini = min(mini, prices[i]);
+//         }
+
+//         return profit;
+//     }
+// };
