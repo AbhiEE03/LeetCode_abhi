c185bf5e4733d16099eee07319f33d25005056f5
Abhishek <abhisheknoni78@gmail.com>
2026-06-03 22:45:11 +0530

    Time: 0 ms (100%), Space: 10.3 MB (33.8%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,15 +1,47 @@
+// // //// BRUTE //// //
+// // WE create an ans matrix with all zeros(n*n)
+// // It passes all the test cases but has SC:O(n^2)
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         // assigning the n rows and n cols with all zeroes
+//         vector<vector<int>> ans(n, vector<int>(n,0));
+
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j < n; j++){
+//                 // each i will become j and j will become n-1-i after rotstion 
+//                 ans[j][n-1-i] = matrix[i][j];
+//             }
+//         }
+//         // Reassign ans to matrix
+//         matrix = ans;
+        
+//     }
+// };
+
+
+
+// //// OPTIMAL //// //
+// WE can do it by taking TRANSPOSE then REVERSING each row
+// For Transpose: We need to swap diagonally opposite elements in both halves
+// while the primary diagonal elements remain where they are
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> ans(n, vector<int>(n,0));
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                ans[j][n-1-i] = matrix[i][j];
+        // Transpose
+        for(int i = 0; i < n-1; i++){
+            for(int j = i+1; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        matrix = ans;
-        
+
+        // Reverse each row
+        for(int i = 0; i < n; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
