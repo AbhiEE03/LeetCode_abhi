726e5273ea20a2dfb2e2a8ec918de8d10f03e4bf
Abhishek <abhisheknoni78@gmail.com>
2026-06-07 00:25:25 +0530

    Time: 0 ms (100%), Space: 23.5 MB (81.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,28 +1,71 @@
+//  //// OPTIMAL //// //
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> ans;
-        unordered_map<int ,int> mpp;
+        int cnt1 = 0, cnt2 = 0, n = nums.size();
+        int majEl1 = INT_MIN, majEl2 = INT_MIN;
+        int minOccurrence = (int)(n/3) + 1;
 
-        // Element is appearing atleast (n/3 + 1) times
-        int mn=(n/3)+1;
+        for(int i = 0; i < nums.size(); i++){
+            if(cnt1 == 0 && majEl2 != nums[i]){
+                cnt1++;
+                majEl1 = nums[i];
+            }else if(cnt2 == 0 && majEl1 != nums[i]){
+                cnt2++;
+                majEl2 = nums[i];
+            }else if(majEl1 == nums[i]){
+                cnt1++;
+            }else if(majEl2 == nums[i]){
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
+        }
 
-        for(int i=0;i<n;i++){
-           mpp[nums[i]]++;
+        vector<int> majorityEl;
+        cnt1 = 0; cnt2 = 0;
 
-            // Add it to the ans array as the count reaches (n/3 + 1)
-           if(mpp[nums[i]]==mn)
-                ans.push_back(nums[i]);
-
-            // We can have atmost 2 elements
-            if(ans.size() == 2)
-                break;
+        for(int i = 0; i < n; i++){
+            if(majEl1 == nums[i])cnt1++;
+            if(majEl2 == nums[i])cnt2++;    
 
         }
-        return ans;
+
+        if(cnt1 >= minOccurrence) majorityEl.push_back(majEl1);
+        if(cnt2 >= minOccurrence) majorityEl.push_back(majEl2);
+        return majorityEl;
     }
 };
+
+
+// // //// HASHING //// //
+// // TC: O(n) SC: O(n)
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> ans;
+//         unordered_map<int ,int> mpp;
+
+//         // Element is appearing atleast (n/3 + 1) times
+//         int mn=(n/3)+1;
+
+//         for(int i=0;i<n;i++){
+//            mpp[nums[i]]++;
+
+//             // Add it to the ans array as the count reaches (n/3 + 1)
+//            if(mpp[nums[i]]==mn)
+//                 ans.push_back(nums[i]);
+
+//             // We can have atmost 2 elements
+//             if(ans.size() == 2)
+//                 break;
+
+//         }
+//         return ans;
+//     }
+// };
 
 
 
