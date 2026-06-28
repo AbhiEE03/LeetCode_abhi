dff406c9820c51415866498401820bba6dd66f7c
Abhishek <abhisheknoni78@gmail.com>
2026-06-29 00:35:45 +0530

    Time: 19 ms (74.53%), Space: 139.1 MB (66.79%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,11 +5,16 @@ public:
         vector<int> ans;
         deque<int> dq;
         for (int i = 0; i < n; i++) {
+            // Indices <= i-k are out of window so pop them
             if (!dq.empty() && dq.front() <= i - k)
                 dq.pop_front();
+            
+            // Smaller elements will never contribute to the answer so keep large ones only
             while (!dq.empty() && nums[dq.back()] <= nums[i])
                 dq.pop_back();
             dq.push_back(i);
+
+            // This means the window is complete so the front element will be the answer
             if (i >= k - 1)
                 ans.push_back(nums[dq.front()]);
         }
