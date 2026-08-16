655c408b354ea3c1a08ff1a58d68001d8d27c942
Abhishek <abhisheknoni78@gmail.com>
2026-08-16 17:41:57 +0530

    Time: 127 ms (59.96%), Space: 17.4 MB (60.6%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,3 +1,46 @@
+// 2nd Attempt (Infosys)//
+class Solution {
+private:
+    bool subset(vector<int>& nums, int val){
+        int n = nums.size();
+        vector<vector<bool>> dp(n+1, vector<bool> (val+1, false));
+
+        // If reqSum == 0 then it's always possible
+        for(int i =0; i<n+1; i++){
+            if(i == 0)
+                dp[i][0] = true;
+        }
+
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < val+1; j++){
+                if(nums[i-1] <= j)
+                    dp[i][j] = (dp[i-1][j - nums[i-1]] || dp[i-1][j]);
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][val];
+
+    }
+
+public:
+    bool canPartition(vector<int>& nums) {
+
+        // It's possible only when the reqSum is even 
+        int sum = 0;
+        for(auto &it: nums){
+            sum += it;
+        }
+        int reqSum = sum/2;
+        if(sum % 2 != 0)
+            return false;
+        else
+            return subset(nums, reqSum);
+    }
+};                                              
+
+/*
+// //// Fist Attempt //// // 
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
@@ -36,3 +79,4 @@ public:
         return dp[n][S1];
     }
 };
+*/
