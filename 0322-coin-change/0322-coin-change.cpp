cdfc0d7f1039a564d843e78a4c81c7428b74abb3
Abhishek <abhisheknoni78@gmail.com>
2026-08-23 18:41:47 +0530

    Time: 19 ms (81.01%), Space: 17.9 MB (84.28%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -8,20 +8,22 @@ public:
         // dp[x] = final subproblem
 
         // To get zero sum u need 0 coins
-        // For every amount initialise with very large val as if it's imp to reach
+        // For every amount initialise with very large val as if it's imp to
+        // reach
         vector<int> dp(amount + 1, 1e9);
 
         // Base case = smallest subproblem that needs to be solved
         dp[0] = 0;
 
-        for(int i = 1; i<=amount; i++){
-            for(int j = 0; j<n ; j++){
-                if(coins[j] <= i)
+        // for(int i = 1; i<=amount; i++){
+        //     for(int j = 0; j<n ; j++){
+        for (int j = 0; j < n; j++) {
+            for (int i = 1; i <= amount; i++) {
+                if (coins[j] <= i)
                     dp[i] = min(dp[i - coins[j]] + 1, dp[i]);
             }
         }
         return dp[amount] == 1e9 ? -1 : dp[amount];
-
     }
 };
 
@@ -30,7 +32,7 @@ public:
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        
+
         int n = coins.size();
         vector<vector<int>> dp(n+1, vector<int> (amount+1, 0));
 
@@ -41,7 +43,7 @@ public:
 
         for(int i=1; i<n+1; i++){
             for(int j=1; j<amount+1; j++){
-            
+
             // The coin giving valid ans can be taken again
             if(coins[i-1] <= j)
                 dp[i][j] = min(dp[i][j-coins[i-1]] + 1, dp[i-1][j]);
